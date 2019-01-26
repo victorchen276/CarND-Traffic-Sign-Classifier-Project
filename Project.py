d44737adfcdd43708c0ccbf64a4e2e91f295ebd3
Victor Chen <victorchen276@gmail.com>
2019-01-26 23:25:06 +1300

    add lenet lab solution

--- Project.py
+++ Project.py
@@ -103,3 +103,31 @@ plt.show()
 
 # X_train_one_label = X_train[np.where(y_train == 0)]
 # draw_images_examples(X_train_one_label, 16, 4, 'Examples of images of the same type - Speed limit (20km/h)')
+
+
+import numpy as np
+from sklearn.utils import shuffle
+from skimage import exposure
+
+
+def preprocess_dataset(X, y=None):
+    # Convert to grayscale, e.g. single Y channel
+    X = 0.299 * X[:, :, :, 0] + 0.587 * X[:, :, :, 1] + 0.114 * X[:, :, :, 2]
+    # Scale features to be in [0, 1]
+    X = (X / 255.).astype(np.float32)
+
+    # Apply localized histogram localization
+    for i in range(X.shape[0]):
+        X[i] = exposure.equalize_adapthist(X[i])
+
+    if y is not None:
+        # Convert to one-hot encoding. Convert back with `y = y.nonzero()[1]`
+        y = np.eye(43)[y]
+        # Shuffle the data
+        X, y = shuffle(X, y)
+
+    # Add a single grayscale channel
+    X = X.reshape(X.shape + (1,))
+    return X, y
+
+
