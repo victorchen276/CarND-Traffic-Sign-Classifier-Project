95e9c5a23e5b6c13645c2b4d86f600fea46ab241
Victor Chen <victorchen276@gmail.com>
2019-02-01 23:14:44 +1300

    add training result files

--- LeNetLab.py
+++ LeNetLab.py
@@ -53,6 +53,10 @@ image = X_train[index].squeeze()
 
 plt.figure(figsize=(1,1))
 plt.imshow(image, cmap="gray")
+
+plt.tight_layout()
+plt.show()
+
 print(y_train[index])
 
 from sklearn.utils import shuffle
@@ -65,206 +69,206 @@ The EPOCH and BATCH_SIZE values affect the training speed and model accuracy.
 You do not need to modify this section.
 '''
 
-
-import tensorflow as tf
-
-EPOCHS = 10
-BATCH_SIZE = 128
-
-
-'''
-SOLUTION: Implement LeNet-5
-Implement the LeNet-5 neural network architecture.
-
-This is the only cell you need to edit.
-
-Input
-The LeNet architecture accepts a 32x32xC image as input, where C is the number of color channels. Since MNIST images are grayscale, C is 1 in this case.
-
-Architecture
-Layer 1: Convolutional. The output shape should be 28x28x6.
-
-Activation. Your choice of activation function.
-
-Pooling. The output shape should be 14x14x6.
-
-Layer 2: Convolutional. The output shape should be 10x10x16.
-
-Activation. Your choice of activation function.
-
-Pooling. The output shape should be 5x5x16.
-
-Flatten. Flatten the output shape of the final pooling layer such that it's 1D instead of 3D. The easiest way to do is by using tf.contrib.layers.flatten, which is already imported for you.
-
-Layer 3: Fully Connected. This should have 120 outputs.
-
-Activation. Your choice of activation function.
-
-Layer 4: Fully Connected. This should have 84 outputs.
-
-Activation. Your choice of activation function.
-
-Layer 5: Fully Connected (Logits). This should have 10 outputs.
-
-Output
-Return the result of the 2nd fully connected layer.
-'''
-
-from tensorflow.contrib.layers import flatten
-
-
-def LeNet(x):
-    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
-    mu = 0
-    sigma = 0.1
-
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
-    conv1_b = tf.Variable(tf.zeros(6))
-    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
-
-    # SOLUTION: Activation.
-    conv1 = tf.nn.relu(conv1)
-
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-
-    # SOLUTION: Activation.
-    conv2 = tf.nn.relu(conv2)
-
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0 = flatten(conv2)
-
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
-    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
-
-    # SOLUTION: Activation.
-    fc1 = tf.nn.relu(fc1)
-
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
-    fc2_b = tf.Variable(tf.zeros(84))
-    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
-
-    # SOLUTION: Activation.
-    fc2 = tf.nn.relu(fc2)
-
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 10), mean=mu, stddev=sigma))
-    fc3_b = tf.Variable(tf.zeros(10))
-    logits = tf.matmul(fc2, fc3_W) + fc3_b
-
-    return logits
-
-'''
-Features and Labels
-Train LeNet to classify MNIST data.
-
-x is a placeholder for a batch of input images. y is a placeholder for a batch of output labels.
-
-You do not need to modify this section.
-'''
-
-x = tf.placeholder(tf.float32, (None, 32, 32, 1))
-y = tf.placeholder(tf.int32, (None))
-one_hot_y = tf.one_hot(y, 10)
-
-'''
-Training Pipeline
-Create a training pipeline that uses the model to classify MNIST data.
-
-You do not need to modify this section.
-'''
-
-rate = 0.001
-
-logits = LeNet(x)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation)
-
-
-'''
-Model Evaluation
-Evaluate how well the loss and accuracy of the model for a given dataset.
-
-You do not need to modify this section.
-'''
-
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-saver = tf.train.Saver()
-
-def evaluate(X_data, y_data):
-    num_examples = len(X_data)
-    total_accuracy = 0
-    sess = tf.get_default_session()
-    for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
-        total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
-
-'''
-Train the Model
-Run the training data through the training pipeline to train the model.
-
-Before each epoch, shuffle the training set.
-
-After each epoch, measure the loss and accuracy of the validation set.
-
-Save the model after training.
-
-You do not need to modify this section.
-'''
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train)
-
-    print("Training...")
-    print()
-    for i in range(EPOCHS):
-        X_train, y_train = shuffle(X_train, y_train)
-        for offset in range(0, num_examples, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-
-        validation_accuracy = evaluate(X_validation, y_validation)
-        print("EPOCH {} ...".format(i + 1))
-        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
-        print()
-
-    saver.save(sess, './lenet')
-    print("Model saved")
-
-
-'''
-Evaluate the Model
-Once you are completely satisfied with your model, evaluate the performance of the model on the test set.
-
-Be sure to only do this once!
-
-If you were to measure the performance of your trained model on the test set, then improve your model, and then measure the performance of your model on the test set again, that would invalidate your test results. You wouldn't get a true measure of how well your model would perform against real data.
-
-You do not need to modify this section.
-'''
-
-
-with tf.Session() as sess:
-    saver.restore(sess, tf.train.latest_checkpoint('.'))
-
-    test_accuracy = evaluate(X_test, y_test)
-    print("Test Accuracy = {:.3f}".format(test_accuracy))
+#
+# import tensorflow as tf
+#
+# EPOCHS = 10
+# BATCH_SIZE = 128
+#
+#
+# '''
+# SOLUTION: Implement LeNet-5
+# Implement the LeNet-5 neural network architecture.
+#
+# This is the only cell you need to edit.
+#
+# Input
+# The LeNet architecture accepts a 32x32xC image as input, where C is the number of color channels. Since MNIST images are grayscale, C is 1 in this case.
+#
+# Architecture
+# Layer 1: Convolutional. The output shape should be 28x28x6.
+#
+# Activation. Your choice of activation function.
+#
+# Pooling. The output shape should be 14x14x6.
+#
+# Layer 2: Convolutional. The output shape should be 10x10x16.
+#
+# Activation. Your choice of activation function.
+#
+# Pooling. The output shape should be 5x5x16.
+#
+# Flatten. Flatten the output shape of the final pooling layer such that it's 1D instead of 3D. The easiest way to do is by using tf.contrib.layers.flatten, which is already imported for you.
+#
+# Layer 3: Fully Connected. This should have 120 outputs.
+#
+# Activation. Your choice of activation function.
+#
+# Layer 4: Fully Connected. This should have 84 outputs.
+#
+# Activation. Your choice of activation function.
+#
+# Layer 5: Fully Connected (Logits). This should have 10 outputs.
+#
+# Output
+# Return the result of the 2nd fully connected layer.
+# '''
+#
+# from tensorflow.contrib.layers import flatten
+#
+#
+# def LeNet(x):
+#     # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+#     mu = 0
+#     sigma = 0.1
+#
+#     # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+#     conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
+#     conv1_b = tf.Variable(tf.zeros(6))
+#     conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+#
+#     # SOLUTION: Activation.
+#     conv1 = tf.nn.relu(conv1)
+#
+#     # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
+#     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+#
+#     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
+#     conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+#     conv2_b = tf.Variable(tf.zeros(16))
+#     conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+#
+#     # SOLUTION: Activation.
+#     conv2 = tf.nn.relu(conv2)
+#
+#     # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+#     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+#
+#     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+#     fc0 = flatten(conv2)
+#
+#     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
+#     fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
+#     fc1_b = tf.Variable(tf.zeros(120))
+#     fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+#
+#     # SOLUTION: Activation.
+#     fc1 = tf.nn.relu(fc1)
+#
+#     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
+#     fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+#     fc2_b = tf.Variable(tf.zeros(84))
+#     fc2 = tf.matmul(fc1, fc2_W) + fc2_b
+#
+#     # SOLUTION: Activation.
+#     fc2 = tf.nn.relu(fc2)
+#
+#     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
+#     fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 10), mean=mu, stddev=sigma))
+#     fc3_b = tf.Variable(tf.zeros(10))
+#     logits = tf.matmul(fc2, fc3_W) + fc3_b
+#
+#     return logits
+#
+# '''
+# Features and Labels
+# Train LeNet to classify MNIST data.
+#
+# x is a placeholder for a batch of input images. y is a placeholder for a batch of output labels.
+#
+# You do not need to modify this section.
+# '''
+#
+# x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+# y = tf.placeholder(tf.int32, (None))
+# one_hot_y = tf.one_hot(y, 10)
+#
+# '''
+# Training Pipeline
+# Create a training pipeline that uses the model to classify MNIST data.
+#
+# You do not need to modify this section.
+# '''
+#
+# rate = 0.001
+#
+# logits = LeNet(x)
+# cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+# loss_operation = tf.reduce_mean(cross_entropy)
+# optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+# training_operation = optimizer.minimize(loss_operation)
+#
+#
+# '''
+# Model Evaluation
+# Evaluate how well the loss and accuracy of the model for a given dataset.
+#
+# You do not need to modify this section.
+# '''
+#
+# correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+# accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# saver = tf.train.Saver()
+#
+# def evaluate(X_data, y_data):
+#     num_examples = len(X_data)
+#     total_accuracy = 0
+#     sess = tf.get_default_session()
+#     for offset in range(0, num_examples, BATCH_SIZE):
+#         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+#         accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+#         total_accuracy += (accuracy * len(batch_x))
+#     return total_accuracy / num_examples
+#
+# '''
+# Train the Model
+# Run the training data through the training pipeline to train the model.
+#
+# Before each epoch, shuffle the training set.
+#
+# After each epoch, measure the loss and accuracy of the validation set.
+#
+# Save the model after training.
+#
+# You do not need to modify this section.
+# '''
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     num_examples = len(X_train)
+#
+#     print("Training...")
+#     print()
+#     for i in range(EPOCHS):
+#         X_train, y_train = shuffle(X_train, y_train)
+#         for offset in range(0, num_examples, BATCH_SIZE):
+#             end = offset + BATCH_SIZE
+#             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+#             sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+#
+#         validation_accuracy = evaluate(X_validation, y_validation)
+#         print("EPOCH {} ...".format(i + 1))
+#         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+#         print()
+#
+#     saver.save(sess, './lenet')
+#     print("Model saved")
+#
+#
+# '''
+# Evaluate the Model
+# Once you are completely satisfied with your model, evaluate the performance of the model on the test set.
+#
+# Be sure to only do this once!
+#
+# If you were to measure the performance of your trained model on the test set, then improve your model, and then measure the performance of your model on the test set again, that would invalidate your test results. You wouldn't get a true measure of how well your model would perform against real data.
+#
+# You do not need to modify this section.
+# '''
+#
+#
+# with tf.Session() as sess:
+#     saver.restore(sess, tf.train.latest_checkpoint('.'))
+#
+#     test_accuracy = evaluate(X_test, y_test)
+#     print("Test Accuracy = {:.3f}".format(test_accuracy))
--- Project.py
+++ Project.py
@@ -8,74 +8,55 @@ import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
-# TODO: Fill this in based on where you saved the training and testing data
-
-training_file = "./traffic-signs-data/train.p"
-validation_file = "./traffic-signs-data/valid.p"
-testing_file = "./traffic-signs-data/test.p"
-
-with open(training_file, mode='rb') as f:
-    train = pickle.load(f)
-with open(validation_file, mode='rb') as f:
-    valid = pickle.load(f)
-with open(testing_file, mode='rb') as f:
-    test = pickle.load(f)
-
-X_train, y_train = train['features'], train['labels']
-X_valid, y_valid = valid['features'], valid['labels']
-X_test, y_test = test['features'], test['labels']
-
-
-
-### Replace each question mark with the appropriate value.
-### Use python, pandas or numpy methods rather than hard coding the results
-
-# TODO: Number of training examples
-n_train = y_train.shape[0]
-
-# TODO: Number of validation examples
-n_validation = y_valid.shape[0]
-
-# TODO: Number of testing examples.
-n_test = y_test.shape[0]
-
-# TODO: What's the shape of an traffic sign image?
-image_shape = X_train[0].shape
-
-# TODO: How many unique classes/labels there are in the dataset.
-sign_classes, class_indices, class_counts = np.unique(y_train, return_index=True, return_counts=True)
-
-n_classes = class_counts.shape[0]
-
-
-print("Number of training examples =", n_train)
-print("Number of testing examples =", n_test)
-print("Image data shape =", image_shape)
-print("Number of classes =", n_classes)
-
-
-# signnames = read_csv("signnames.csv").values[:, 1]
-# col_width = max(len(name) for name in signnames)
+# # TODO: Fill this in based on where you saved the training and testing data
 #
-# for c, c_index, c_count in zip(sign_classes, class_indices, class_counts):
-#     print("Class %i: %-*s  Count: %s " % (c, col_width, signnames[c], str(c_count)))
-#     fig = pyplot.figure(figsize=(6, 1))
-#     fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
-#     random_indices = random.sample(range(c_index, c_index + c_count), 10)
-#     for i in range(5):
-#         axis = fig.add_subplot(1, 10, i + 1, xticks=[], yticks=[])
-#         axis.imshow(X_train[random_indices[i]])
-#     pyplot.show()
+# training_file = "./traffic-signs-data/train.p"
+# validation_file = "./traffic-signs-data/valid.p"
+# testing_file = "./traffic-signs-data/test.p"
 #
-# pyplot.bar(np.arange(43), class_counts, align='center')
-# pyplot.xlabel('Class')
-# pyplot.ylabel('Number of training examples')
-# pyplot.xlim([-1, 43])
-# pyplot.show()
-
-# get_ipython().magic('matplotlib inline')
-
-
+# with open(training_file, mode='rb') as f:
+#     train = pickle.load(f)
+# with open(validation_file, mode='rb') as f:
+#     valid = pickle.load(f)
+# with open(testing_file, mode='rb') as f:
+#     test = pickle.load(f)
+#
+# X_train, y_train = train['features'], train['labels']
+# X_valid, y_valid = valid['features'], valid['labels']
+# X_test, y_test = test['features'], test['labels']
+#
+#
+#
+# ### Replace each question mark with the appropriate value.
+# ### Use python, pandas or numpy methods rather than hard coding the results
+#
+name_values = np.genfromtxt('signnames.csv', skip_header=1, dtype=[('myint','i8'), ('mysring','S55')], delimiter=',')
+#
+# # TODO: Number of training examples
+# n_train = y_train.shape[0]
+#
+# # TODO: Number of validation examples
+# n_validation = y_valid.shape[0]
+#
+# # TODO: Number of testing examples.
+# n_test = y_test.shape[0]
+#
+# # TODO: What's the shape of an traffic sign image?
+# image_shape = X_train[0].shape
+#
+# # TODO: How many unique classes/labels there are in the dataset.
+# sign_classes, class_indices, class_counts = np.unique(y_train, return_index=True, return_counts=True)
+#
+# n_classes = class_counts.shape[0]
+#
+#
+# print("Number of training examples =", n_train)
+# print("Number of testing examples =", n_test)
+# print("Image data shape =", image_shape)
+# print("Number of classes =", n_classes)
+#
+#
+#
 def draw_images_examples(image_array, grid_x, grid_y, title):
     fig = plt.figure(figsize=(grid_x, grid_y))
     fig.suptitle(title, fontsize=20)
@@ -89,28 +70,47 @@ def draw_images_examples(image_array, grid_x, grid_y, title):
         plt.axis('off')
 
     plt.show()
+#
+#
+# draw_images_examples(X_train, 16, 4, 'Examples of images from training set')
+#
+# fig = plt.figure(figsize=(12, 4))
+# n, bins, patches = plt.hist(y_train, n_classes)
+# plt.xlabel('Labels')
+# plt.ylabel('No. of samples')
+# plt.title('Histogram of training samples')
+# plt.show()
+#
+#
+# # X_train_one_label = X_train[np.where(y_train == 0)]
+# # draw_images_examples(X_train_one_label, 16, 4, 'Examples of images of the same type - Speed limit (20km/h)')
+#
+#
+# import numpy as np
+# from sklearn.utils import shuffle
+# from skimage import exposure
+#
+#
+def plot_figures(figures, nrows=1, ncols=1, labels=None):
+    fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(12, 14))
+    axs = axs.ravel()
+    for index, title in zip(range(len(figures)), figures):
+        axs[index].imshow(figures[title], plt.gray())
+        if (labels != None):
+            axs[index].set_title(labels[index])
+        else:
+            axs[index].set_title(title)
 
+        axs[index].set_axis_off()
 
-draw_images_examples(X_train, 16, 4, 'Examples of images from training set')
+    plt.tight_layout()
+    plt.show()
 
-fig = plt.figure(figsize=(12, 4))
-n, bins, patches = plt.hist(y_train, n_classes)
-plt.xlabel('Labels')
-plt.ylabel('No. of samples')
-plt.title('Histogram of training samples')
-plt.show()
-
-
-# X_train_one_label = X_train[np.where(y_train == 0)]
-# draw_images_examples(X_train_one_label, 16, 4, 'Examples of images of the same type - Speed limit (20km/h)')
-
-
-import numpy as np
-from sklearn.utils import shuffle
 from skimage import exposure
-
+from sklearn.utils import shuffle
 
 def preprocess_dataset(X, y=None):
+    print('preprocess_dataset')
     # Convert to grayscale, e.g. single Y channel
     X = 0.299 * X[:, :, :, 0] + 0.587 * X[:, :, :, 1] + 0.114 * X[:, :, :, 2]
     # Scale features to be in [0, 1]
@@ -131,3 +131,67 @@ def preprocess_dataset(X, y=None):
     return X, y
 
 
+def load_pickled_data(file, columns):
+    print('load_pickled_data')
+    with open(file, mode='rb') as f:
+        dataset = pickle.load(f)
+    return tuple(map(lambda c: dataset[c], columns))
+
+def load_and_process_data(pickled_data_file):
+    print('load_and_process_data')
+    X, y = load_pickled_data(pickled_data_file, columns=['features', 'labels'])
+    X, y = preprocess_dataset(X, y)
+    return (X, y)
+
+
+training_file = "./traffic-signs-data/train.p"
+validation_file = "./traffic-signs-data/valid.p"
+testing_file = "./traffic-signs-data/test.p"
+
+training_file_preprocessed_file = './traffic-signs-data/train_preprocessed.p'
+validation_file_preprocessed_file = './traffic-signs-data/valid_preprocessed.p'
+test_file_preprocessed_file = './traffic-signs-data/test_preprocessed.p'
+
+
+X_train_gray, y_train = load_pickled_data(training_file_preprocessed_file, columns=['features', 'labels'])
+# X_valid_gray, _ = preprocess_dataset(X_valid, y_train)
+# X_test_gray, _ = preprocess_dataset(X_test, y_train)
+#
+number_to_stop = 8
+figures = {}
+labels = {}
+random_signs = []
+n_train = X_train_gray.shape[0]
+for i in range(number_to_stop):
+    index = random.randint(0, n_train - 1)
+    # labels[i] = name_values[y_train[index]][1].decode('ascii')
+    figures[i] = X_train_gray[index].squeeze()
+    random_signs.append(index)
+
+print(random_signs)
+plot_figures(figures, 4, 2, labels=None)
+
+# draw_images_examples(X_train_gray, 16, 4, 'Examples of images from training set')
+
+
+
+# X_train, y_train = load_and_process_data(training_file)
+# pickle.dump({
+#         "features" : X_train,
+#         "labels" : y_train
+#     }, open(training_file_preprocessed_file, "wb"))
+# print("Preprocessed balanced training dataset saved in", training_file_preprocessed_file)
+#
+# X_train, y_train = load_and_process_data(validation_file)
+# pickle.dump({
+#         "features":X_train,
+#         "labels":y_train
+#     }, open(validation_file_preprocessed_file, "wb" ) )
+# print("Preprocessed extended training dataset saved in", validation_file_preprocessed_file)
+#
+# X_test, y_test = load_and_process_data(testing_file)
+# pickle.dump({
+#         "features":X_test,
+#         "labels":y_test
+#     }, open(test_file_preprocessed_file, "wb" ) )
+# print("Preprocessed extended testing dataset saved in", test_file_preprocessed_file)
